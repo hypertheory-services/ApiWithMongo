fde4f78345c3723f1f6cc8aa08e2e363f6d52bc8
Jeffry Gonzalez <jeff@hypertheory.com>
2022-05-12 08:28:15 -0400

    Add project files.

---
+++ Domain/Document.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ApiWithMongo.Domain;
+
+public class Document
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+}
